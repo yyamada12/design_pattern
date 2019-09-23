d969230b56334941764881c7506025622dfd1979
yyamada12 <12yacropolisy@gmail.com>
2019-09-23 10:44:28 +0900

    adapter 2

---
+++ adapter/Hanako.java
@@ -0,0 +1,9 @@
+public class Hanako implements Chairperson {
+    private Taro taro;
+    public Hanako() {
+        taro = new Taro();
+    }
+    public void organizeClass() {
+        taro.enjoyWithAllClassmate();
+    }
+}
